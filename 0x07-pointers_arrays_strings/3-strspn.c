207153bcfd76278cbce1a1b4c52c0f9e5568ce31
Nyamisa <priscah.moms@gmail.com>
2022-09-27 03:34:41 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,26 +1,38 @@
 #include "main.h"
 /**
- *_strchr - Locates a character in a string
+ *_strspn - Locates a character in a string
  *
- *@s: String
+ *@s: This is the main C string to be scanned.
  *
- *c: Character to search
+ *@accept: This is the string containing the list of characters to match in s
  *
- *Return: Pointer to the first occurrence of the character c in the string s
+ *Return: return count
  **/
-char *_strchr(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
 {
-while (*s != '\0') /*Declaring WHILE*/
+int i, j;
+int count = 0;
+char *str1, *str2;
+str1 = s;
+str2 = accept;
+i = 0;
+while (str1[i] != '\0') /*Declaring WHILE *s */
 {
-if (*s == c) /*if s ==  c*/
+j = 0;
+while (str2[j] != '\0') /*Declaring WHILE *accept*/
 {
-return (s); /*return s*/
+if (str2[j] == str1[i]) /*Evaluate condition*/
+{
+count++; /*count number*/
+break;
 }
-++s;
+j++;    /*add j+1*/
 }
-if (*s == c)
+if (s[i] != accept[j]) /*If aren't equals*/
 {
-return (s);
+break;
 }
-return (0);/* values null*/
+i++; /*add x+1*/
+}
+return (count); /*return the value of count*/
 }
